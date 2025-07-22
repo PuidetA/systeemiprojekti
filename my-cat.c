14b8d1507d09c3f175b93a2801006426ed852588
PuidetA <aapuidet@gmail.com>
2025-07-22 19:31:59 +0300

    1. Improve error handling by replacing fprintf with perror for file access errors. 2. Added newline check for fgets

--- my-cat.c
+++ my-cat.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[]) {
     //Checking to see if any files have actually been provided to read
     if (argc < 2) {
-        fprintf(stderr, "Please provide files to search.\n");
+        perror("Error: please provide files to search.\n");
         exit(1);
     }
 
@@ -15,13 +15,18 @@ int main(int argc, char *argv[]) {
         FILE *currentfile = fopen(argv[i], "r");
         //Check if the program can open the file
         if (currentfile == NULL) {
-            fprintf(stderr, "Cannot open file.");
+            perror("Error: cannot open file.");
             exit(1); //If a file can't be opened, the program exits.
         }
         //Printing out the file; help for code courtesy of https://www.geeksforgeeks.org/c/c-program-print-contents-file/
-        char buffer[1000];
+        char buffer[16*1024];
         while (fgets(buffer, sizeof(buffer), currentfile) != NULL) {
             printf("%s", buffer);
+
+            size_t len = strlen(buffer); // If no newline is detected at the end of each line, it will print a newline.
+            if (len > 0 && buffer[len-1] == "\n") {
+                printf("\n");
+            }
         }
         fclose(currentfile); //Closing the file
         i++; //Increasing the value of i to move to the next file
